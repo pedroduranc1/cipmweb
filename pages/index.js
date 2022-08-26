ff0601ef3676c47be636930ff63adb6604c09fdf
Pedro Duran <cipm3gh@gmail.com>
2022-08-26 10:09:19 -0400

    pd

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Incio - CIMP</title>
+        <title>Inicio - CIMP</title>
         <link rel="icon" href="logo.svg"/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
