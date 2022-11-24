dc327fe5de535d4e5f3061650986ed96ee3db9ba
pedroduranc1 <61203515+pedroduranc1@users.noreply.github.com>
2022-11-24 10:45:58 -0400

    Update Plans.jsx

--- components/Plans.jsx
+++ components/Plans.jsx
@@ -33,7 +33,7 @@ const Plans = () => {
       }
 
       setEnviandoCorreoPlans(true)
-      emailjs.send('service_9jc938i', 'template_ba8iocf', valuesemaill, '3Z8BU-k9bSAu5bHtx')
+      emailjs.send('service_6nyy3ia', 'template_ba8iocf', valuesemaill, '3Z8BU-k9bSAu5bHtx')
         .then(function (response) {
           swal(`Felicidades ${valuesemaill.nombrecompleto}`, "Tu correo a sido enviado con exito", "success");
           limpiarinp()
