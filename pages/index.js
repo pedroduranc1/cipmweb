86c56d0082c11448efa07cf6c1f44214b51348e9
pedroduranc1 <61203515+pedroduranc1@users.noreply.github.com>
2022-11-25 10:15:57 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio - CIMP</title>
+        <title>Inicio - CIPM</title>
         <link rel="icon" href="logo.svg"/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
